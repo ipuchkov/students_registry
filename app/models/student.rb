782c9399cd2af985ebbd3f55f0b171a88c319ef3
Ivan Puchkov <rake.eclipse@gmail.com>
2015-08-28 18:40:34 +0600

    add queries for search and duplicated ip

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,9 +7,20 @@ class Student < ActiveRecord::Base
   has_many   :average_marks, :dependent => :destroy
 
   scope :ordered, -> { order(:surname, :name) }
+  #топ 10 студентов курса в семестре
   scope :top,     ->(course_id, semester_number) { includes(:course, :group, :average_marks => [:semester]).
                                                    where(:courses => { :id => course_id }, :semesters => { :number => semester_number }).
-                                                   order('average_marks.value desc, students.surname, students.name') }
+                                                   order('average_marks.value desc, students.surname, students.name').limit(10) }
+
+  #студенты с одинаковым ip адресом и как минимум одной характеристикой преподавателя
+  scope :duplicate_ip_with_characteristic, -> { where("ip in (SELECT ip FROM students GROUP BY ip HAVING count(ip) > 1 and count(characteristic) > 0)").order('ip') }
+
+  #однокурсники с средним баллом между ... и ... в семестре и с именем %имя%
+  scope :for_search, ->(course_id, semester_number, min, max, name) { joins(:course, :average_marks => [:semester]).
+                                                                      where(:courses => { :id => course_id }, :semesters => { :number => semester_number }).
+                                                                      where("average_marks.value BETWEEN #{min} AND #{max}").
+                                                                      where("students.name LIKE '%#{name}%'")
+  }
 
   normalize_attribute :email
 
