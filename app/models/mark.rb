adf7e84105b3bca014fe7de81e2baf1a7640d0ce
Ivan Puchkov <rake.eclipse@gmail.com>
2015-08-29 11:56:53 +0600

    little code styling

--- app/models/mark.rb
+++ app/models/mark.rb
@@ -3,9 +3,15 @@ class Mark < ActiveRecord::Base
   belongs_to :subject
   belongs_to :student
 
-  scope :by_semester,             ->(semester_id) { joins(:semester).where(:semesters => { :id => semester_id }) }
-  scope :by_subject,              ->(subject_id)  { joins(:subject).where(:subjects => { :id => subject_id }) }
-  scope :by_semester_and_subject, ->(semester_id, subject_id) { joins(:semester, :subject).where(:semesters => { :id => semester_id }, :subjects => { :id => subject_id }) }
+  scope :by_semester, ->(id) do
+    joins(:semester).where(:semesters => { :id => id })
+  end
+  scope :by_subject, ->(id) do
+    joins(:subject).where(:subjects => { :id => id })
+  end
+  scope :by_semester_and_subject, ->(semester_id, subject_id) do
+    by_semester(semester_id).by_subject(subject_id)
+  end
 
   after_create  :increase_average_mark
   after_destroy :decrease_average_mark
--- app/models/student.rb
+++ app/models/student.rb
@@ -8,20 +8,29 @@ class Student < ActiveRecord::Base
 
   scope :ordered, -> { order(:surname, :name) }
 
-  #топ 10 студентов курса в семестре
-  scope :top, ->(course_id, semester_number) { includes(:course, :group, :average_marks => [:semester]).
-                                               where(:courses => { :id => course_id }, :semesters => { :number => semester_number }).
-                                               order('average_marks.value desc, students.surname, students.name').limit(10) }
+  # топ 10 студентов курса в семестре
+  scope :top, ->(course_id, semester_number) do
+    includes(:course, :group, :average_marks => [:semester]).
+      where(:courses => { :id => course_id }, :semesters => { :number => semester_number }).
+      order('average_marks.value desc, students.surname, students.name').limit(10)
+  end
 
-  #студенты с одинаковым ip адресом и как минимум одной характеристикой преподавателя
-  scope :duplicate_ip_with_characteristic, -> { where("ip in (SELECT ip FROM students GROUP BY ip HAVING count(ip) > 1 and count(characteristic) > 0)").order('ip') }
+  # студенты с одинаковым ip адресом и как минимум одной характеристикой преподавателя
+  scope :duplicate_ip_with_characteristic, -> do
+    where("ip in (
+          SELECT ip FROM students
+          GROUP BY ip
+          HAVING count(ip) > 1 AND count(characteristic) > 0)").
+      order('ip')
+  end
 
-  #однокурсники с средним баллом между min и max в семестре и с именем name
-  scope :for_search, ->(course_id, semester_number, min, max, name) { joins(:course, :average_marks => [:semester]).
-                                                                      where(:courses => { :id => course_id }, :semesters => { :number => semester_number }).
-                                                                      where("average_marks.value BETWEEN #{min} AND #{max}").
-                                                                      where("LOWER(students.name) LIKE LOWER('#{name}')")
-  }
+  # однокурсники с средним баллом между min и max в семестре и с именем name
+  scope :for_search, ->(course_id, semester_number, min, max, name) do
+    joins(:course, :average_marks => [:semester]).
+      where(:courses => { :id => course_id }, :semesters => { :number => semester_number }).
+      where("average_marks.value BETWEEN #{min} AND #{max}").
+      where("LOWER(students.name) LIKE LOWER('#{name}')")
+  end
 
   normalize_attribute :email
 
@@ -32,12 +41,12 @@ class Student < ActiveRecord::Base
     [surname, name].join(' ')
   end
 
-  #оценка по предмету в семестре
+  # оценка по предмету в семестре
   def mark_for(semester_id, subject_id)
     marks.by_semester_and_subject(semester_id, subject_id).first
   end
 
-  #средний балл в семестре
+  # средний балл в семестре
   def average_mark_for(semester_id)
     average_marks.find_by(:semester_id => semester_id)
   end
